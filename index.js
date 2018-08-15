5c3b8c8ab595664d1d52f34093d414c1324aefdc
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-08-15 15:45:05 +0900

    graphql study coding

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema} from "graphql-tools"
 // console.log("start")
 
-const app = express();
+//더미 데이터 
+const books = [
+    {title:"문제는 경제다", author:"김계동"},
+    {title:"82년생", author:"공지영"},
+    {title:"88세대", author:"유시민"},
+]
 
-app.get('/',(req,res)=>{
-    res.send("하하하호호호")
-})
+const typeDefs = `
+    type Query{
+        books : [Book]
+    }
+    type Book{
+        title:String
+        author:String
+    }
+`
+const schema = makeExecutableSchema ({
+    typeDefs,
+})  
+
+const app = express();
+// app.get('/',(req,res)=>{
+//     res.send("하하하호호호")
+// })
+
+app.use("/graphql", bodyParser.json(), graphqlExpress({schema}))
+
+app.use("/graphiql", graphiqlExpress({endpointURL:"/graphql"}))
+
 app.listen(3000, ()=>{
     console.log("서버가 시작되었습니다!")
-})
+}) 
